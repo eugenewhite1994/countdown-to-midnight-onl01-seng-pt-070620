96bfc654a93e4a64f0671c48303668983dd784c8
Eugene White <eugenewhite1994@gmail.com>
2020-07-09 18:57:39 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,14 +1,11 @@
 def countdown(number)
   while number > 0
   puts "#{number} SECONDS!"
-number += 1
-until number == 10
-  break
-end
+number -= 1
 end
 end
 
-countdown(0)
+countdown(10)
 
 
 def countdown_with_sleep
