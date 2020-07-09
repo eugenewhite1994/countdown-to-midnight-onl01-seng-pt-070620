4136123b6072d4d67469b6724128545549df534a
Eugene White <eugenewhite1994@gmail.com>
2020-07-09 18:40:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
-#write your code here
-
-def countdown
+def countdown(number)
+  while number > 0
+  puts "#{number} SECONDS!"
+number += 1
+  break
 end
+end
+
+countdown(9)
+
+
+def countdown_with_sleep
+  sleep 10
+  puts "Happy New Year! "
+end
+countdown_with_sleep
